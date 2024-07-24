b252918ff954fe6d190b5578ed255a51bda8d8fc
Muhammad Bilal Noshahi <mbilalnoshahi@hotmail.com>
2024-07-25 01:25:48 +0500

    Created Signup form simple page - It is doing nothing yet

--- AddedDotNetFrameWorkSupportToCpp/MainWindow.h
+++ AddedDotNetFrameWorkSupportToCpp/MainWindow.h
@@ -26,96 +26,124 @@ namespace Project1 {
             }
         }
 
-    private: System::Windows::Forms::Button^ myButton;
-    private: System::Windows::Forms::Label^ myLabel;
-    private: System::Windows::Forms::Panel^ mainPanel;
-    private: System::Windows::Forms::PictureBox^ logoPictureBox;
-    private: System::Windows::Forms::Label^ developerLabel;  // Added for developer info
+    private: System::Windows::Forms::TextBox^ usernameTextBox;
+    private: System::Windows::Forms::TextBox^ passwordTextBox;
+    private: System::Windows::Forms::TextBox^ emailTextBox;
+    private: System::Windows::Forms::Label^ usernameLabel;
+    private: System::Windows::Forms::Label^ passwordLabel;
+    private: System::Windows::Forms::Label^ emailLabel;
+    private: System::Windows::Forms::Button^ signupButton;
+    private: System::Windows::Forms::Label^ developerLabel;
     private: System::ComponentModel::Container^ components;
 
 #pragma region Windows Form Designer generated code
            void InitializeComponent(void)
            {
-               this->myButton = (gcnew System::Windows::Forms::Button());
-               this->myLabel = (gcnew System::Windows::Forms::Label());
-               this->mainPanel = (gcnew System::Windows::Forms::Panel());
-               this->logoPictureBox = (gcnew System::Windows::Forms::PictureBox());
-               this->developerLabel = (gcnew System::Windows::Forms::Label());  // Initialize developerLabel
-               this->mainPanel->SuspendLayout();
-               (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->logoPictureBox))->BeginInit();
+               this->usernameTextBox = (gcnew System::Windows::Forms::TextBox());
+               this->passwordTextBox = (gcnew System::Windows::Forms::TextBox());
+               this->emailTextBox = (gcnew System::Windows::Forms::TextBox());
+               this->usernameLabel = (gcnew System::Windows::Forms::Label());
+               this->passwordLabel = (gcnew System::Windows::Forms::Label());
+               this->emailLabel = (gcnew System::Windows::Forms::Label());
+               this->signupButton = (gcnew System::Windows::Forms::Button());
+               this->developerLabel = (gcnew System::Windows::Forms::Label());
+
                this->SuspendLayout();
                // 
-               // myButton
+               // usernameTextBox
                // 
-               this->myButton->Location = System::Drawing::Point(100, 150);
-               this->myButton->Name = L"myButton";
-               this->myButton->Size = System::Drawing::Size(100, 50);
-               this->myButton->TabIndex = 0;
-               this->myButton->Text = L"Click Me";
-               this->myButton->UseVisualStyleBackColor = true;
-               this->myButton->BackColor = System::Drawing::Color::LightSkyBlue;
-               this->myButton->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
-               this->myButton->Click += gcnew System::EventHandler(this, &MyForm::myButton_Click);
+               this->usernameTextBox->Location = System::Drawing::Point(150, 50);
+               this->usernameTextBox->Name = L"usernameTextBox";
+               this->usernameTextBox->Size = System::Drawing::Size(200, 22);
+               this->usernameTextBox->TabIndex = 0;
                // 
-               // myLabel
+               // passwordTextBox
                // 
-               this->myLabel->AutoSize = true;
-               this->myLabel->Location = System::Drawing::Point(100, 220);
-               this->myLabel->Name = L"myLabel";
-               this->myLabel->Size = System::Drawing::Size(46, 17);
-               this->myLabel->TabIndex = 1;
-               this->myLabel->Text = L"Hello!";
-               this->myLabel->Font = (gcnew System::Drawing::Font(L"Arial", 10));
+               this->passwordTextBox->Location = System::Drawing::Point(150, 100);
+               this->passwordTextBox->Name = L"passwordTextBox";
+               this->passwordTextBox->Size = System::Drawing::Size(200, 22);
+               this->passwordTextBox->TabIndex = 1;
+               this->passwordTextBox->UseSystemPasswordChar = true;
                // 
-               // mainPanel
+               // emailTextBox
                // 
-               this->mainPanel->Controls->Add(this->myButton);
-               this->mainPanel->Controls->Add(this->myLabel);
-               this->mainPanel->Controls->Add(this->logoPictureBox);
-               this->mainPanel->Controls->Add(this->developerLabel);  // Add developerLabel to the panel
-               this->mainPanel->Location = System::Drawing::Point(12, 12);
-               this->mainPanel->Name = L"mainPanel";
-               this->mainPanel->Size = System::Drawing::Size(460, 300);  // Increased size for better visibility
-               this->mainPanel->TabIndex = 2;
-               this->mainPanel->BackColor = System::Drawing::Color::WhiteSmoke;
+               this->emailTextBox->Location = System::Drawing::Point(150, 150);
+               this->emailTextBox->Name = L"emailTextBox";
+               this->emailTextBox->Size = System::Drawing::Size(200, 22);
+               this->emailTextBox->TabIndex = 2;
                // 
-               // logoPictureBox
+               // usernameLabel
                // 
-               this->logoPictureBox->Location = System::Drawing::Point(100, 20);
-               this->logoPictureBox->Name = L"logoPictureBox";
-               this->logoPictureBox->Size = System::Drawing::Size(100, 100);
-               this->logoPictureBox->TabIndex = 3;
-               this->logoPictureBox->TabStop = false;
-               this->logoPictureBox->Image = Image::FromFile(L"coding.png");
+               this->usernameLabel->AutoSize = true;
+               this->usernameLabel->Location = System::Drawing::Point(50, 50);
+               this->usernameLabel->Name = L"usernameLabel";
+               this->usernameLabel->Size = System::Drawing::Size(77, 17);
+               this->usernameLabel->TabIndex = 3;
+               this->usernameLabel->Text = L"Username:";
+               // 
+               // passwordLabel
+               // 
+               this->passwordLabel->AutoSize = true;
+               this->passwordLabel->Location = System::Drawing::Point(50, 100);
+               this->passwordLabel->Name = L"passwordLabel";
+               this->passwordLabel->Size = System::Drawing::Size(73, 17);
+               this->passwordLabel->TabIndex = 4;
+               this->passwordLabel->Text = L"Password:";
+               // 
+               // emailLabel
+               // 
+               this->emailLabel->AutoSize = true;
+               this->emailLabel->Location = System::Drawing::Point(50, 150);
+               this->emailLabel->Name = L"emailLabel";
+               this->emailLabel->Size = System::Drawing::Size(46, 17);
+               this->emailLabel->TabIndex = 5;
+               this->emailLabel->Text = L"Email:";
+               // 
+               // signupButton
+               // 
+               this->signupButton->Location = System::Drawing::Point(150, 200);
+               this->signupButton->Name = L"signupButton";
+               this->signupButton->Size = System::Drawing::Size(200, 50);
+               this->signupButton->TabIndex = 6;
+               this->signupButton->Text = L"Sign Up";
+               this->signupButton->UseVisualStyleBackColor = true;
+               this->signupButton->Click += gcnew System::EventHandler(this, &MyForm::signupButton_Click);
                // 
                // developerLabel
                // 
                this->developerLabel->AutoSize = true;
-               this->developerLabel->Location = System::Drawing::Point(100, 250);  // Adjusted position
+               this->developerLabel->Location = System::Drawing::Point(50, 300);
                this->developerLabel->Name = L"developerLabel";
-               this->developerLabel->Size = System::Drawing::Size(250, 20);  // Adjusted size
-               this->developerLabel->TabIndex = 4;
+               this->developerLabel->Size = System::Drawing::Size(250, 17);
+               this->developerLabel->TabIndex = 7;
                this->developerLabel->Text = L"Developed by Muhammad Bilal Noshahi";
-               this->developerLabel->Font = (gcnew System::Drawing::Font(L"Arial", 10));  // Font size
-               this->developerLabel->ForeColor = System::Drawing::Color::Gray;  // Optional color
+               this->developerLabel->Font = (gcnew System::Drawing::Font(L"Arial", 10));
+               this->developerLabel->ForeColor = System::Drawing::Color::Gray;
+
                // 
                // MyForm
                // 
                this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
                this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-               this->ClientSize = System::Drawing::Size(484, 361);  // Increased ClientSize
-               this->Controls->Add(this->mainPanel);
+               this->ClientSize = System::Drawing::Size(400, 400);
+               this->Controls->Add(this->usernameTextBox);
+               this->Controls->Add(this->passwordTextBox);
+               this->Controls->Add(this->emailTextBox);
+               this->Controls->Add(this->usernameLabel);
+               this->Controls->Add(this->passwordLabel);
+               this->Controls->Add(this->emailLabel);
+               this->Controls->Add(this->signupButton);
+               this->Controls->Add(this->developerLabel);
                this->Name = L"MyForm";
-               this->Text = L"MyForm";
-               this->mainPanel->ResumeLayout(false);
-               this->mainPanel->PerformLayout();
-               (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->logoPictureBox))->EndInit();
+               this->Text = L"Sign Up";
                this->ResumeLayout(false);
+               this->PerformLayout();
            }
 #pragma endregion
 
-    private: System::Void myButton_Click(System::Object^ sender, System::EventArgs^ e) {
-        this->myLabel->Text = L"Button Clicked!";
+    private: System::Void signupButton_Click(System::Object^ sender, System::EventArgs^ e) {
+        MessageBox::Show("Sign Up button clicked!");
+        // You can add more logic here for signup processing.
     }
     };
 }
